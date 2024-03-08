6b9187e8555df61c12283b1a8ded0f5b1c5ad3a1
dippanz <emanuele110@tiscali.it>
2024-03-08 14:43:21 +0100

    resolve bug for secure access

--- src/pages/Profile/Profile.jsx
+++ src/pages/Profile/Profile.jsx
@@ -2,7 +2,9 @@ import { useContext } from "react";
 import "./Profile.css";
 import { AuthContext } from "../../components/context/AuthContextProvider";
 import imageProfile from "../../assets/icon-profile.png";
-import UsersList from "../UsersList/UsersList.jsx";
+import UsersList from "../../components/Users/UsersList.jsx";
+import ErrorPage from "../ErrorPage/ErrorPage.jsx";
+import { Link } from "react-router-dom";
 
 export default function Profile() {
   const { user, setUser } = useContext(AuthContext);
@@ -11,37 +13,48 @@ export default function Profile() {
   return (
     <>
       {/* questa parte serve per qualsiasi utente loggato, sotto metto solo quella per gli admin */}
-      <div className="containerProfile">
-        <div className="containerImageProfile">
-          <img src={imageProfile} alt="profile image" />
-        </div>
+      {isLogged ? (
+        <div>
+          <div className="containerProfile">
+            <div className="containerImageProfile">
+              <img src={imageProfile} alt="profile image" />
+            </div>
 
-        <div className="containerDataProfile">
-          <p>
-            <b>First Name:</b> {user.firstName}{" "}
-          </p>
-          <p>
-            <b>Last Name:</b> {user.lastName}
-          </p>
-          <p>
-            <b>Email:</b> {user.email}{" "}
-          </p>
+            <div className="containerDataProfile">
+              <p>
+                <b>First Name:</b> {user.firstName}{" "}
+              </p>
+              <p>
+                <b>Last Name:</b> {user.lastName}
+              </p>
+              <p>
+                <b>Email:</b> {user.email}{" "}
+              </p>
 
-          <div>
-            <p>
-              <b>Ruoli:</b>
-              {user.ruoli.map((role) => (
-                <span key={role.id != null ? role.id : Math.random()}>
-                  {" "}
-                  {role.tipologia},
-                </span>
-              ))}
-            </p>
+              <div>
+                <p>
+                  <b>Ruoli:</b>
+                  {user.ruoli.map((role) => (
+                    <span key={role.id != null ? role.id : Math.random()}>
+                      {" "}
+                      {role.tipologia},
+                    </span>
+                  ))}
+                </p>
+              </div>
+            </div>
           </div>
-        </div>
-      </div>
 
-      <UsersList></UsersList>
+          <UsersList></UsersList>
+        </div>
+      ) : (
+        <ErrorPage
+          messageError="Sessione di autenticazione scaduta o non effettuata"
+          paraError="Per favore, effettua il login per accedere a questa pagina."
+        >
+          <Link to="/login">Vai alla pagina di login</Link>
+        </ErrorPage>
+      )}
     </>
   );
 }
